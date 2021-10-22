27d09991ab6e99102021f8172a6b0ec673acf716
Rohit Satwadhar <rvsatwadhar@gmail.com>
2021-10-22 21:49:08 +0530

    Create a simple Apache kafka consumer and producer.

---
+++ src/main/java/com/invictus/kafkatwitter/TwitterConsumer.java
@@ -0,0 +1,119 @@
+package com.invictus.kafkatwitter;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class TwitterConsumer {
+    public static void main(String[] args) {
+
+        new TwitterConsumer().run();
+    }
+
+    public TwitterConsumer() {
+
+    }
+
+    public void run()  {
+
+        Logger logger = LoggerFactory.getLogger(TwitterConsumer.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "twitter-application";
+        String topic = "twitter_tweets";
+        // Set properties
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+
+        ConsumerThread consumerThread = new ConsumerThread(
+                bootstrapServers,
+                groupId,
+                topic,
+                latch
+        );
+
+        Thread myThread=new Thread(consumerThread);
+        myThread.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            logger.info("Caught ShutDown Hook");
+            consumerThread.shutdown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("Application exited ");
+        }));
+
+        try {
+
+            // sleep this thread until latch is non-zero
+            // when latch becomes zero awake this thread or when there is any interruption
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application got interrupted");
+        }finally {
+            logger.info("application exited");
+        }
+    }
+
+    public static class ConsumerThread implements Runnable {
+
+        private CountDownLatch latch;
+
+        Logger logger = LoggerFactory.getLogger(TwitterConsumer.class);
+
+        private KafkaConsumer<String, String> consumer;
+
+        public ConsumerThread(String bootstrapServers, String groupId, String topic, CountDownLatch latch) {
+            this.latch = latch;
+
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+            consumer = new KafkaConsumer<String, String>(properties);
+            consumer.subscribe(Arrays.asList(topic));
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                    for (ConsumerRecord<String, String> record : records) {
+                        logger.info("Key: " + record.key() + ", value: " + record.value());
+                        logger.info("Partition: " + record.partition() + " offset: " + record.offset());
+                    }
+                }
+            } catch (WakeupException e) {
+                logger.info("Received shutdown signal");
+            } finally {
+                consumer.close();
+                latch.countDown();
+            }
+
+        }
+
+        public void shutdown() {
+            // throws WakeUpException
+            consumer.wakeup();
+        }
+    }
+}
---
+++ src/main/java/com/invictus/kafkatwitter/TwitterProducer.java
@@ -0,0 +1,162 @@
+package com.invictus.kafkatwitter;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+
+    private static InputStream inputStream;
+    Logger logger= LoggerFactory.getLogger(TwitterProducer.class.getName());
+
+
+    // Keys required for twitter stream
+    static String consumerKey;
+    static String consumerSecret;
+    static String token;
+    static String secret;
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        readConfigValues();
+        new TwitterProducer().run();
+
+    }
+
+    private static void readConfigValues() throws FileNotFoundException {
+
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
+
+        inputStream = TwitterProducer.class.getClassLoader().getResourceAsStream(propFileName);
+
+        if (inputStream != null) {
+            try {
+                prop.load(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+        consumerKey=prop.getProperty("consumerKey");
+        consumerSecret=prop.getProperty("consumerSecret");
+        secret=prop.getProperty("secret");
+        token=prop.getProperty("token");
+
+    }
+
+    public void run(){
+
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+
+        // Create twitter client
+        Client twitterClient=createTwitterClient(msgQueue);
+
+        twitterClient.connect();
+
+        KafkaProducer<String,String>producer=createKafkaProducer();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            logger.info("Stopping Application...");
+            logger.info("Shutting Down client from twitter");
+            twitterClient.stop();
+            logger.info("Closed application");
+            producer.close();
+        }));
+        while (!twitterClient.isDone()) {
+            String msg = null;
+
+            // Get Msg. from twitter
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                twitterClient.stop();
+            }
+
+            // Send Msg. to kafka
+            if(msg!=null){
+                logger.info(msg);
+                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if(e!=null){
+                            logger.error("Something bad happened");
+
+                        }
+                    }
+                });
+            }
+
+        }
+        logger.info("End of application");
+        // Create Kafka Producer
+
+
+        // Loop to send tweets to kafka
+    }
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue ){
+
+        /* Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+// Optional: set up some followings and track terms
+        List<String> terms = Lists.newArrayList("India");
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        return builder.build();
+
+    }
+
+    public KafkaProducer<String,String>createKafkaProducer(){
+        String bootstrapServer="127.0.0.1:9092";
+
+        // Set Config properties
+        Properties properties=new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG,"true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5");
+        // Kafka 2.0 >=1.1 so we can keep this as 5. Use 1 otherwise
+
+        // Create Producer
+
+        return new KafkaProducer<String, String>(properties);
+    }
+}
